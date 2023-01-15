bf48e7718f4acf1964bb5eda374dd45e4a6560e3
abdumurod9911 <khasanboyevabdumurod2@gmail.com>
2023-01-16 02:44:44 +0500

    fix

--- 06.01.23/index.js
+++ 06.01.23/index.js
@@ -1,5 +1,152 @@
-// 15.01.23
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 05.01.2023
+// 11.Return the Remainder from Two Numbers
+function remainder(x, y) {
+	return x % y;
+}
+//12.Return Something to Me!
+function giveMeSomething(a) {
+	return `something ${a}`; 
+}
+
+// 06.01.2023
+// 13.Is the Number Less than or Equal to Zero?
+function lessThanOrEqualToZero(num) {
+	if(num <= 0){
+		return true;
+	}else{
+		return false;
+	}
+}
+// 14.Basketball Points
+function points(twoPointers, threePointers) {
+	return (twoPointers * 2)+(threePointers * 3);
+}
+
+
+
+// 07.01.2023
+// 15.Less Than 100?
+function lessThan100(a, b) {
+	if(a + b < 100){
+		return true;
+	}else {
+		return false;
+	}
+}
+// 16.Using the "&&" Operator
+function and(a, b) {
+	if(a && b == true){
+		return true
+	}else {
+		return false
+	}
+}
+
+
+// 08.01.2023
+// 17.Football Points
+function footballPoints(wins, draws, losses) {
+	return(wins * 3) + (draws * 1) + (losses * 0 );
+}
+// 18.Fix the Expression
+function isSeven(x) {
+	return x=== 7;
+}
+
+
+// 09.01.2023
+// 19.Sum of Polygon Angles
+function sumPolygon(n) {
+	if(n>2)
+		return (n-2)*180;
+}
+//20.Basic Variable Assignment
+function nameString(name){
+	var b = "Edabit"
+	var result = name + b
+  	return result
+}
+
+
+// 10.01.2023
+// 21.The Farm Problem
+function animals(chickens, cows, pigs) {
+	return (chickens * 2 ) + (cows * 4) + (pigs * 4);
+}
+// 22.Are the Numbers Equal?
+function isSameNum(num1, num2) {
+	if(num1 === num2){
+		return true; 
+	}else {
+		return false;
+	}
+}
+
+
+
+// 12.01.2023
+// 23.Convert Hours and Minutes into Seconds
+function convert(hours, minutes) {
+	return (hours * 3600)+(minutes * 60);  
+}
+// 24.Equality Check
+function checkEquality(a, b) {
+	if(a === b){
+		  return true;
+	  }else {
+	  return false;	
+	  }
+  }
+
+
+
+
+// 13.01.2023
+// 25.Sum of Polygon Angles -----------Ko'pburchak burchaklar yig'indisi
+function sumPolygon(n) {
+	if (n>2);
+		return (n-2)*180;
+	}
+// 26.Basic Variable Assignment
+function nameString(name){
+	var b = "Edabit"
+	var result = name + b
+  	return result
+}
+
+
+
+// 14.01.2023
+// 27.The Farm Problem ------ Fermer xo'jaligi muammosi
+function animals(chickens, cows, pigs) {
+	return (chickens * 2) + (cows * 4) + (pigs * 4);
+}
+// 28.Are the Numbers Equal?
+function isSameNum(num1, num2) {
+	if(num1 === num2){
+		return true
+	}else {
+		return false
+	}
+}
+
+
+
+// 15.01.23
 // 29.Convert Hours and Minutes into Seconds
 
 function convert(hours, minutes) {
@@ -14,3 +161,5 @@ function checkEquality(a, b) {
 		  return false;
 	  }
   }
+
+  
