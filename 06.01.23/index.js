e9653552205c89188347f0c12205416287d7d314
abdumurod9911 <khasanboyevabdumurod2@gmail.com>
2023-01-16 10:51:33 +0500

    fix

--- 06.01.23/index.js
+++ 06.01.23/index.js
@@ -1,18 +1,55 @@
+// 06.01.2023
+// 1.Return the Sum of Two Numbers
+function addition(a, b) {
+	return a + b;
+}
 
+// 2.Return the Next Number from the Integer Passed
+function addition(num) {
+	return num + 1;
+}
 
+// 3.Convert Age to Days
+function calcAge(age) {
+	return age * 365;
+}
 
+// 4.Power Calculator
+function circuitPower(voltage, current) {
+	return voltage * current;
+}
 
+// 5.Maximum Edge of a Triangle
+function nextEdge(side1, side2) {
+	return (side1 + side2)-1;
+}
 
+// 6.Find the Perimeter of a Rectangle
+function findPerimeter(length, width) {
+	return length *2 + width * 2;
+}
 
+// 07.01.2023
+// 7.Convert Minutes into Seconds
+function convert(minutes) {
+	return minutes * 60;
+}
 
+// 8.Area of a Triangle
+function triArea(base, height) {
+	return (base * height) / 2;
+}
 
+// 9.Return the First Element in an Array
+function getFirstValue(arr) {
+	return arr[0];
+}
 
+// 10.Convert Hours into Seconds
+function howManySeconds(hours) {
+	return hours * 3600;
+	}
 
-
-
-
-
-// 05.01.2023
 // 11.Return the Remainder from Two Numbers
 function remainder(x, y) {
 	return x % y;
@@ -22,7 +59,7 @@ function giveMeSomething(a) {
 	return `something ${a}`; 
 }
 
-// 06.01.2023
+// 08.01.2023
 // 13.Is the Number Less than or Equal to Zero?
 function lessThanOrEqualToZero(num) {
 	if(num <= 0){
@@ -36,9 +73,6 @@ function points(twoPointers, threePointers) {
 	return (twoPointers * 2)+(threePointers * 3);
 }
 
-
-
-// 07.01.2023
 // 15.Less Than 100?
 function lessThan100(a, b) {
 	if(a + b < 100){
@@ -56,8 +90,6 @@ function and(a, b) {
 	}
 }
 
-
-// 08.01.2023
 // 17.Football Points
 function footballPoints(wins, draws, losses) {
 	return(wins * 3) + (draws * 1) + (losses * 0 );
